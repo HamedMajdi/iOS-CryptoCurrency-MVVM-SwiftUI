525064ce60babb3492c5b3cefdd1161929d5e271
hamedmajdi <hamed90_viva@yahoo.com>
2024-02-22 15:46:47 +0300

    Initial Commit

---
+++ Crypto Currency - MVVM & SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Crypto Currency - MVVM & SwiftUI
+//
+//  Created by Hamed Majdi on 2/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Crypto Currency - MVVM & SwiftUI/Crypto_Currency___MVVM___SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_Currency___MVVM___SwiftUIApp.swift
+//  Crypto Currency - MVVM & SwiftUI
+//
+//  Created by Hamed Majdi on 2/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_Currency___MVVM___SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
